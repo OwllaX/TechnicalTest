bb58dd5fa780207e173169144d31a655d8a27b8b
OwllaX <axel72avs@outlook.com>
2020-07-23 11:57:53 -0600

    Get JSON data to be used in the table.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TechnicalTest';
+
+  url1 = 'http://northwind.netcore.io/query/customers.json';
+  url2 = 'http://northwind.netcore.io/orders.json';
+
+  customers = [];
+  orders = [];
+
+  countCustomers = 0;
+  countOrders = 0;
+
+  constructor(private http1: HttpClient, private http2: HttpClient) {
+    this.http1.get(this.url1).toPromise().then(data => {
+
+      for (let key in data['results']) {
+        if (data['results'].hasOwnProperty(key)) {
+          this.countCustomers ++;
+        }
+      }
+
+      for (let index = 0; index < this.countCustomers; index++) {
+        this.customers.push(data.results[index]);
+      }
+
+      console.log(this.customers);
+    });
+
+    this.http2.get(this.url2).toPromise().then(data => {
+      
+      for (let key in data['results']) {
+        if (data['results'].hasOwnProperty(key)) {
+          this.countOrders ++;          
+        }
+      }
+
+      for (let index = 0; index < this.countOrders; index++) {
+        this.orders.push(data.results[index]);
+      }
+
+      console.log(this.orders);
+    });
+  }  
 }
